8173831b3d71cb402f2c2f9ed55bcfd09f76024d
Silenrate <daniwalteros596@gmail.com>
2021-02-19 11:50:16 -0500

    Basic App Configuration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+  const HomeView = (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+          </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+          </a>
+    </header>
+  );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Route exact path="/" component={HomeView} />
+      </Router>
     </div>
   );
 }
