44fbd288f4e42441e7ac19424f16a3dd384b7527
NicolasAguilera9906 <nicolasaguileracontreras@gmail.com>
2021-02-21 22:12:40 -0500

    sign up design form , functionality and data validation using Formik and Yup

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,66 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { Page_Header } from './global-components/page-header';
 import { Footer_v1 } from './global-components/footer';
 import { Navbar } from './global-components/navbar';
-import { LoginView} from './section-components/LoginView';
+import { Box, Paper, Tab, Tabs, Typography } from '@material-ui/core';
+import { LoginView } from './section-components/LoginView';
+import { RegisterView } from './section-components/RegisterView';
+
 
 export const Login = () =>{
+
+
+    const [value, setValue] = useState(0);
+
+    const handleChange = (event,newValue) =>{
+        setValue(newValue);
+    };
+
+    const paperStyle = {width:310,margin:"20px auto"};
+
+
+    function TabPanel(props) {
+        const { children, value, index, ...other } = props;
+
+        return (
+          <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+          >
+            {value === index && (
+              <Box >
+                <Typography>{children}</Typography>
+              </Box>
+            )}
+          </div>
+        );
+      }
 
     return (
         <div>
         <Navbar />
         <Page_Header HeaderTitle={"Welcome to Envirify"}/>
-        <LoginView></LoginView>  
+        <Paper elevation={20} style={paperStyle}>
+            <Tabs
+                value={value}
+                indicatorColor="primary"
+                textColor="primary"
+                onChange={handleChange}
+                aria-label="disabled tabs example"
+            >
+                <Tab label="Sign in" />
+                <Tab label="Sign up" />
+            </Tabs>
+            <TabPanel value={value} index={0}>
+                <LoginView handleChange={handleChange}></LoginView>
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                <RegisterView></RegisterView>
+            </TabPanel>
+        </Paper>
         <Footer_v1 />
         </div>
     );
---
+++ src/components/section-components/RegisterView.js
@@ -0,0 +1,158 @@
+import { Avatar, Button, Checkbox, FormControl, FormControlLabel, FormHelperText, FormLabel, Grid, Paper, Radio, RadioGroup, TextField, Typography } from '@material-ui/core';
+import React from 'react';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import Swal from 'sweetalert2';
+
+
+export const RegisterView = () => {
+
+    const paperStyle = { padding: '30px 20px', width: 310, margin: "0px auto" };
+
+    const headerStyle = { margin: 0 };
+
+    const avatarStyle = { backgroundColor: '#1bbd7e' }
+
+    const marginTop = { marginTop: 5 }
+
+    const initialValues = {
+        name: '',
+        email: '',
+        gender: '',
+        phoneNumber: '',
+        password: '',
+        confirmPassword: ''
+    }
+
+    const onSubmit = (values, props) => {
+        
+        setTimeout(() => {
+            props.resetForm();
+            props.setSubmitting(false);
+            Swal.fire({
+                title: 'User registered!',
+                text: 'Continue',
+                icon: 'success',
+                confirmButtonText: 'Cool'
+            });
+        }, 2000)
+    }
+
+
+
+    const validationSchema = Yup.object().shape({
+        name: Yup.string().min(3, "It's too short!").required("Required"),
+        email: Yup.string().email("Enter valid email").required("Required"),
+        gender: Yup.string().oneOf(["male", "female"], "Required").required("Required"),
+        phoneNumber: Yup.number().typeError("Enter valid phone number").required("Required"),
+        password: Yup.string().min(9, "Password minimum length should be 8").required("Required"),
+        confirmPassword: Yup.string().oneOf([Yup.ref('password')], "Password not matched").required("Required")
+    })
+
+    return (
+        <Grid>
+            <Paper style={paperStyle}>
+                <Grid align="center">
+                    <Avatar style={avatarStyle}>
+                        <AddCircleOutlineIcon />
+                    </Avatar>
+                    <h3 style={headerStyle}>Sign up</h3>
+                    <Typography
+                        variant='caption'
+                        gutterBottom
+                    >
+                        Please fill this form to create an account
+                        </Typography>
+                </Grid>
+                <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+                    {(props) => {
+                        return (
+                            <Form>
+                                <Field
+                                    as={TextField}
+                                    fullWidth
+                                    name="name"
+                                    label='Name'
+                                    placeholder='Enter your name'
+                                    helperText={
+                                        <ErrorMessage name="name">
+                                            {msg => <div style={{ color: 'red' }}>{msg}</div>}
+                                        </ErrorMessage>}
+                                >
+                                </Field>
+                                <Field
+                                    as={TextField}
+                                    fullWidth
+                                    label='Email'
+                                    name="email"
+                                    placeholder='Enter your Email'
+                                    style={marginTop}
+                                    helperText={
+                                        <ErrorMessage name="email">
+                                            {msg => <div style={{ color: 'red' }}>{msg}</div>}
+                                        </ErrorMessage>}
+                                ></Field>
+                                <Field
+                                    as={TextField}
+                                    fullWidth
+                                    label='Phone number'
+                                    name="phoneNumber"
+                                    placeholder='Enter your phone number'
+                                    style={marginTop}
+                                    helperText={
+                                        <ErrorMessage name="phoneNumber">
+                                            {msg => <div style={{ color: 'red' }}>{msg}</div>}
+                                        </ErrorMessage>}
+                                ></Field>
+                                <FormControl component="fieldset" style={{ marginTop: 20 }}>
+                                    <FormLabel component="legend">Gender</FormLabel>
+                                    <Field as={RadioGroup} aria-label="gender" name="gender" name="gender" style={{ display: 'initial' }}>
+                                        <FormControlLabel value="female" control={<Radio />} label="Female" />
+                                        <FormControlLabel value="male" control={<Radio />} label="Male" />
+                                    </Field>
+                                </FormControl>
+                                <FormHelperText>
+                                    <ErrorMessage name="gender">
+                                        {msg => <div style={{ color: 'red' }}>{msg}</div>}
+                                    </ErrorMessage>
+                                </FormHelperText>
+                                <Field
+                                    as={TextField}
+                                    fullWidth
+                                    label='Password'
+                                    name="password"
+                                    placeholder='Enter your password'
+                                    style={marginTop}
+                                    type="password"
+                                    helperText={
+                                        <ErrorMessage name="password">
+                                            {msg => <div style={{ color: 'red' }}>{msg}</div>}
+                                        </ErrorMessage>}
+                                ></Field>
+                                <Field
+                                    as={TextField}
+                                    fullWidth
+                                    label='Confirm password'
+                                    name="confirmPassword"
+                                    placeholder='Confirm your password'
+                                    style={marginTop}
+                                    type="password"
+                                    helperText={
+                                        <ErrorMessage name="confirmPassword">
+                                            {msg => <div style={{ color: 'red' }}>{msg}</div>}
+                                        </ErrorMessage>}
+                                ></Field>
+                                <Button type='submit' disable={props.isSubmitting} variant='contained' color='primary'>
+                                    {props.isSubmitting ? "Loading" : "Sign up"}
+                                </Button>
+                            </Form>
+                        )
+                    }}
+
+                </Formik>
+            </Paper>
+        </Grid>
+    );
+
+}
